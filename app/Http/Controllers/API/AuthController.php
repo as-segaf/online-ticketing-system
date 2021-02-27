861e5ddd15ce2fcc62fd9790f15c3277e14402fa
as-segaf <ziyanassegaf20@gmail.com>
2021-02-27 10:15:57 +0700

    fix bug auth

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,30 +4,39 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $user = User::where('email', $request->email)->first();
+        // $user = User::where('email', $request->email)->first();
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'message' => 'Unauthorized'
-            ], 401);
+        // if (!$user || !Hash::check($request->password, $user->password)) {
+        //     throw ValidationException::withMessages([
+        //         'email' => 'Email and password does not match.'
+        //     ]);
+        // }
+
+        if (!auth()->attempt($request->only('email','password'))) {
+            throw ValidationException::withMessages([
+                'email' => 'Email and password does not match.'
+            ]);
         }
 
-        $token = $user->createToken('userToken')->plainTextToken;
+        $token = auth()->user()->createToken('userToken')->plainTextToken;
 
         return response()->json([
+            'code' => 200,
             'message' => 'success',
-            'data' => $user,
-            'token' => $token
-        ], 200);
+            'data' => auth()->user(),
+            'token' => $token,
+        ]);
     }
 
     public function logout(Request $request)
