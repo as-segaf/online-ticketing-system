c49b9fcccf096587bde10add93f83e3e20088d81
as-segaf <ziyanassegaf20@gmail.com>
2021-02-27 10:14:43 +0700

    fix middleware

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('api:sanctum')->group(function() {
+Route::middleware('auth:sanctum')->group(function() {
     Route::get('/logout', [AuthController::class, 'logout']);
 });
